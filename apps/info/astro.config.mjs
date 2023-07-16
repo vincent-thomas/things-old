4e2991f0f100616cfc3aa7506155a574f8c85064
Vincent Thomas <beguine-badgast-0p@icloud.com>
2023-07-16 07:56:55 +0000

    la till info sida

---
+++ apps/info/astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+
+import prefetch from '@astrojs/prefetch';
+
+// https://astro.build/config
+export default defineConfig({
+  outDir: '../../dist/apps/info',
+  integrations: [
+    prefetch({
+      intentSelector: 'a',
+    }),
+  ],
+  srcDir: '.',
+});
