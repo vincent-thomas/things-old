d71695f9dccefd95d41ea716322897d463b686cb
Vincent Thomas <beguine-badgast-0p@icloud.com>
2023-07-28 13:02:30 +0200

    fixa cargo lint problem

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -20,40 +20,44 @@ struct Command {
     children: Option<Vec<Command>>,
 }
 
-fn is_flag(maybe_flag: &String) -> bool {
-    maybe_flag.starts_with("-") || maybe_flag.starts_with("--")
+fn is_flag(maybe_flag: &str) -> bool {
+    maybe_flag.starts_with('-') || maybe_flag.starts_with("--")
 }
 
-fn get_root_command(registered: &Vec<Command>, testad: &Vec<String>) -> Option<Command> {
-    let mut base_command: Option<String> = None;
-    let mut base_command_index: Option<usize> = None;
-    for (_, value) in testad.iter().enumerate() {
-        for (index, command_r) in registered.iter().enumerate() {
+fn is_short(command: &str) -> bool {
+    command.len() == 1
+}
+
+fn get_command(registered_commands: &[Command], test_args: &[String]) -> Option<Command> {
+    let mut root_command: Option<String> = None;
+    let mut root_command_index: Option<usize> = None;
+    for (_, value) in test_args.iter().enumerate() {
+        for (index, command_r) in registered_commands.iter().enumerate() {
             let command_name = command_r.name.clone();
-            if value.len() == 1
+            if is_short(value)
                 && command_r.short.is_some()
                 && command_r.short.as_ref().unwrap() == value
             {
-                base_command = Some(command_name);
-                base_command_index = Some(index);
-            } else if &command_r.name == value {
-                base_command = Some(command_name);
-                base_command_index = Some(index);
+                root_command = Some(command_name);
+                root_command_index = Some(index);
+            } else {
+                if &command_r.name == value {
+                    root_command = Some(command_name);
+                    root_command_index = Some(index);
+                }
             }
         }
     }
-    if base_command.is_none() {
-        return None;
-    }
+    root_command?;
 
     let index =
-        base_command_index.expect("Weird error when extracting index of found registered command");
+        root_command_index.expect("Weird error when extracting index of found registered command");
 
-    let root_command = &registered[index];
+    let root_command = &registered_commands[index];
 
     let command_to_send = root_command.clone();
 
-    return Some(command_to_send);
+    Some(command_to_send)
 }
 
 fn main() {
@@ -68,7 +72,7 @@ fn main() {
     {
         let (_, value) = i;
         if is_flag(value) {
-            let flag = value.replace("-", "");
+            let flag = value.replace('-', "");
             flags.push(flag);
         } else {
             commands.push(value.clone())
@@ -87,7 +91,7 @@ fn main() {
 
     let reigstered = vec![testing_command];
 
-    let if_is = get_root_command(&reigstered, &commands);
+    let if_is = get_command(&reigstered, &commands);
     let mut command_string: String = String::from("");
 
     if if_is.is_none() {
@@ -96,13 +100,18 @@ fn main() {
     }
 
     let the_command = if_is.unwrap();
-
     command_string.push_str(the_command.name.as_str());
     if let Some(children) = the_command.children {
         let _ = &commands.remove(0);
-        let sub_command = get_root_command(&children, &commands).unwrap();
-        command_string.push('.');
-        command_string.push_str(sub_command.name.as_str());
+        match get_command(&children, &commands) {
+            Some(sub_command) => {
+                command_string.push('.');
+                command_string.push_str(sub_command.name.as_str());
+            }
+            None => {
+                println!("{:?}", "nooo")
+            }
+        }
     }
 
     println!("{command_string}")
